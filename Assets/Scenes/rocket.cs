83eaf825940a67d7a3ab8629aba4aacb56bc9d91
KR1st <HrabovskyKirill@gmail.com>
2019-03-10 20:54:14 +0300

    Start to work on our rocket

---
+++ Assets/Scenes/rocket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessImput();
+		
+	}
+
+    private void ProcessImput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Space pressed");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            print("Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Right");
+        }
+    }
+}
