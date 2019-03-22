976cc94e2eabfc532313a85c9e1e4478e99d3d78
KR1st <HrabovskyKirill@gmail.com>
2019-03-22 23:16:19 +0300

    Include Audio effect to rocket

--- Assets/Scenes/rocket.cs
+++ Assets/Scenes/rocket.cs
@@ -5,35 +5,46 @@ using UnityEngine;
 
 public class rocket : MonoBehaviour {
 
-    Rigidbody rigidBody; 
-	// Use this for initialization
-	void Start () {
+    Rigidbody rigidBody;
+    AudioSource m_MyAudioSource;
+
+
+    // Use this for initialization
+    void Start () {
         rigidBody = GetComponent<Rigidbody>();
-	}
+        m_MyAudioSource = GetComponent<AudioSource>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        ProcessImput();
-		
+        ProcessInput();
 	}
 
-    private void ProcessImput()
+    private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (m_MyAudioSource.isPlaying == false)
+            {
+                m_MyAudioSource.Play();
+            }
         }
-        else if (Input.GetKey(KeyCode.A))
+        else 
+        {
+            m_MyAudioSource.Stop();
+        }
+        
+         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward);
             print("Left");
-           
         }
         else if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(-Vector3.forward);  
-            print("Right");
-          
+            print("Right");         
         }
     }
 }
